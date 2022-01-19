a474f8f1793ca2b7332fad81009ea0184b34bbe2
Jin Kara <jinlowest@gmail.com>
2022-01-19 22:32:31 +0700

    Update baitapbuoi2.php

--- baitapbuoi2.php
+++ baitapbuoi2.php
@@ -1,58 +1,58 @@
 
 <?php 
 	//Hinh 1:
-	// for ($i=0; $i < 5; $i++) { 
-	// 		echo "<br>";
-	// 	for ($j=0; $j <= $i ; $j++) { 
-	// 		echo "*";
-	// 	}
-	// }
+	for ($i=0; $i < 5; $i++) { 
+			echo "<br>";
+		for ($j=0; $j <= $i ; $j++) { 
+			echo "*";
+		}
+	}
 	//Hinh 2:
-	// for ($i=0; $i < 5; $i++) { 
-	// 		echo "<br>";
-	// 	for ($j=4; $j >= $i ; $j--) { 
-	// 		echo "*";
-	// 	}
-	// }
+	for ($i=0; $i < 5; $i++) { 
+			echo "<br>";
+		for ($j=4; $j >= $i ; $j--) { 
+			echo "*";
+		}
+	}
 	//hinh 3
-	// for ($i = 0; $i < 5; $i++) { 
+	for ($i = 0; $i < 5; $i++) { 
 
-	// 	for ($j = 4; $j > $i ; $j--) { 
-	// 		echo "&nbsp&nbsp";
-	// 	}
+		for ($j = 4; $j > $i ; $j--) { 
+			echo "&nbsp&nbsp";
+		}
 
-	// 	for ($j = 0; $j <= $i ; $j++) { 
-	// 		// code...
-	// 		echo "*";
-	// 	}
-	// 	echo "<br>";
-	// }
+		for ($j = 0; $j <= $i ; $j++) { 
+			// code...
+			echo "*";
+		}
+		echo "<br>";
+	}
 	//Hinh 4:
-	// for ($i = 0; $i < 5; $i++) { 
+	for ($i = 0; $i < 5; $i++) { 
 
-	// 	for ($j = 0; $j < $i ; $j++) { 
-	// 		echo "&nbsp&nbsp";
-	// 	}
+		for ($j = 0; $j < $i ; $j++) { 
+			echo "&nbsp&nbsp";
+		}
 
-	// 	for ($j = $i; $j < 5 ; $j++) { 
-	// 		// code...
-	// 		echo "*";
-	// 	}
-	// 	echo "<br>";
-	// }
+		for ($j = $i; $j < 5 ; $j++) { 
+			// code...
+			echo "*";
+		}
+		echo "<br>";
+	}
 	//Hinh 5:
-	// for ($i = 1; $i < 6; $i++) { 
+	for ($i = 1; $i < 6; $i++) { 
 
-	// 	for ($j = $i; $j < 5 ; $j++) { 
-	// 		echo "&nbsp&nbsp";
-	// 	}
+		for ($j = $i; $j < 5 ; $j++) { 
+			echo "&nbsp&nbsp";
+		}
 
-	// 	for ($j = 1; $j <= (2*$i -1) ; $j++) { 
-	// 		// code...
-	// 		echo "*";
-	// 	}
-	// 	echo "<br>";
-	// }
+		for ($j = 1; $j <= (2*$i -1) ; $j++) { 
+			// code...
+			echo "*";
+		}
+		echo "<br>";
+	}
 	//Hinh 6:
 	for ($i = 1; $i < 6; $i++) { 
 
